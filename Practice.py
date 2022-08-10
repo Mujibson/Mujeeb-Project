c13fde03539e56b9b191e66ffa0dc9830891a30e
Mujibson <110436192+Mujibson@users.noreply.github.com>
2022-08-10 15:46:35 +0100

    Update Practice.py
    5 function calculator using def function

--- Practice.py
+++ Practice.py
@@ -3,3 +3,33 @@ if name == 'Tommy':
     print('You are wrong')
 else:
     print('Alright')
+# Creating a simple calculator
+def addition(): # addition function
+    first_number = int(input('Enter your first number:'))
+    second_number = int(input('Enter your second number:'))
+    print(first_number + second_number)
+addition() 
+
+def subtracton(): #subtraction funcion
+    first_number = int(input('Enter your first number:'))
+    second_number = int(input('Enter your second number:'))
+    print(first_number-second_number)
+subtracton()
+
+def division(): #division funcion
+    first_number = int(input('Enter your first number:'))
+    second_number = int(input('Enter your second number:'))
+    print(first_number/second_number)
+division()
+
+def multiplication(): #multipication funcion
+    first_number = int(input('Enter your first number:'))
+    second_number = int(input('Enter your second number:'))
+    print(first_number*second_number)
+multiplication()
+
+def modulo(): #modulo funcion
+    first_number = int(input('Enter your first number:'))
+    second_number = int(input('Enter your second number:'))
+    print(first_number % second_number)
+modulo()
