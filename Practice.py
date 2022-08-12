ebc9612e109a7a837dd5e74559e4721f54c08360
Mujibson <110436192+Mujibson@users.noreply.github.com>
2022-08-12 17:23:03 +0100

    Update Practice.py
    Modifying Calculator program

--- Practice.py
+++ Practice.py
@@ -8,28 +8,38 @@ def addition(): # addition function
     first_number = int(input('Enter your first number:'))
     second_number = int(input('Enter your second number:'))
     print(first_number + second_number)
-addition() 
 
 def subtracton(): #subtraction funcion
     first_number = int(input('Enter your first number:'))
     second_number = int(input('Enter your second number:'))
     print(first_number-second_number)
-subtracton()
 
 def division(): #division funcion
     first_number = int(input('Enter your first number:'))
     second_number = int(input('Enter your second number:'))
     print(first_number/second_number)
-division()
 
 def multiplication(): #multipication funcion
     first_number = int(input('Enter your first number:'))
     second_number = int(input('Enter your second number:'))
     print(first_number*second_number)
-multiplication()
 
 def modulo(): #modulo funcion
     first_number = int(input('Enter your first number:'))
     second_number = int(input('Enter your second number:'))
     print(first_number % second_number)
-modulo()
+def calc_run(): # modifying calc
+    option = input('add, subtract, divide, multiply, modulo')
+    if option == 'add':
+        addition()
+    elif option == 'subtract':
+        subtracton()
+    elif option == 'divide':
+        division()
+    elif option == 'multiply':
+        multiplication()
+    elif option == 'modulo':
+        modulo()
+    else:
+        print('Please select an option!')
+calc_run()
